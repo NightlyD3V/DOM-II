1d3c7031c3c6a41c7e50472cf95430847c14345c
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-05 22:45:39 +0000

    finished wheel and having an error thrown with drag/drop in index.js

--- js/index.js
+++ js/index.js
@@ -2,9 +2,10 @@
 //            MOUSEOVER NAV
 const navItems = document.querySelectorAll('a');
 // console.log(navItem);
+//       FUNCTION TO CREATE LISTENERS
 function addEventListenerList(list,event,fn) {
     for(let i=0; i<list.length; i++) {
-        list[i].addEventListener(event, fn, false);
+        list[i].addEventListener(event, fn);
     }
 }
 addEventListenerList(navItems, 'mouseover', function() {
@@ -13,7 +14,7 @@ addEventListenerList(navItems, 'mouseover', function() {
 });
 //buggy*
 addEventListenerList(navItems, 'mouseleave', (function(e) {
-    e.preventDefault();
+        e.preventDefault();
         this.style.bottom = "";
     }));
 //            KEYDOWN
@@ -32,3 +33,38 @@ const funBus = document.querySelector('.logo-heading');
 window.addEventListener('keydown', function(e) {
     funBus.style.color = getRandomColor();
 });
+//            WHEEL
+const contentImage = document.getElementsByClassName('img-content');
+console.log(contentImage);
+contentImage[0].addEventListener('wheel', function(e) {
+    contentImage[0].title = "MAPS ARE FUN!";
+});
+contentImage[1].addEventListener('wheel', function(e) {
+    contentImage[1].title = "ADVENTURE!";
+});
+//          DRAG/DROP
+//curently throwing an error;
+function dragstart_handler(event) { //what does ev stand for?
+   //Identify WHAT is draggable
+    console.log('dragStart');
+   event.dataTransfer.setData('image/jps', event.target.id);
+   event.dataTransfer.setData('image/jpg', 'img/destination.jpg');
+   //Drag Image 
+   let img = new Image();
+   img.src = 'img/adventure.jpg';
+   event.dataTransfer.setDragImage(img, 10, 10);
+   //Drop Effect
+   event.dataTransfer.dropEffect = 'copy';
+}
+function dragover_handler(event) {
+    event.preventDefault();
+    //Set the droppEffect to move
+    event.dataTransfer.dropEffect = 'move';
+}
+function drop_handler(event) {
+    event.preventDefault();
+    //Get the ID of the target and add the moved element
+    //to the targets DOM
+    let data = event.dataTransfer.getData('img/jpg');
+    event.target.append(document.getElementById('target'));
+}
