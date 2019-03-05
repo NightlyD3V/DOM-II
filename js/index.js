67d4b4ed68ed4bafd78c9244db58667b5a925ae1
NightlyD3V <TylerSpaulding95@gmail.com>
2019-03-05 21:35:10 +0000

    completed mouseover and keydown eventListeners in index.js

--- js/index.js
+++ js/index.js
@@ -1,1 +1,34 @@
 // Your code goes here
+//            MOUSEOVER NAV
+const navItems = document.querySelectorAll('a');
+// console.log(navItem);
+function addEventListenerList(list,event,fn) {
+    for(let i=0; i<list.length; i++) {
+        list[i].addEventListener(event, fn, false);
+    }
+}
+addEventListenerList(navItems, 'mouseover', function() {
+    this.style.position = "relative";
+    this.style.bottom = "10px";
+});
+//buggy*
+addEventListenerList(navItems, 'mouseleave', (function(e) {
+    e.preventDefault();
+        this.style.bottom = "";
+    }));
+//            KEYDOWN
+// STOLE THIS 'GENERATOR' CODE ONLINE NOT SURE IF IT'S 
+// OUT OF SCOPE TO EXPLAIN
+// HOW IT WORKS
+function getRandomColor() {
+    let letters = '0123456789ABCDEF';
+    let color = '#';
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  }
+const funBus = document.querySelector('.logo-heading');
+window.addEventListener('keydown', function(e) {
+    funBus.style.color = getRandomColor();
+});
